00da694ead61d28268ccdbcc4d0e3672a4e2557d
Samuel Reuben <sreuben1998@gmail.com>
2023-04-17 16:03:33 +0000

    Rock page works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    # Write a ton of code
+
+  # redirect_to("https://www.wikipedia.org")
+  
+  # render({ :html => "<h1>Hello, World!</h1>".html_safe })
+
+  render({ :template => "game_templates/user_rock.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
 
+  get("", { :controller => "application", :action => "homepage" })
+
+get("/rock", { :controller => "application", :action => "play_rock"})
 
 end
