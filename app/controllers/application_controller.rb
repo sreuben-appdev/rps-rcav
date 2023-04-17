8c5a66a1d69b12d3914665b2e1281e0e3a827cea
Samuel Reuben <sreuben1998@gmail.com>
2023-04-17 17:04:48 +0000

    Great progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,19 @@ class ApplicationController < ActionController::Base
 
   render({ :template => "game_templates/user_rock.html.erb" })
   end
+
+  def play_paper
+
+     @comp_move = ["rock", "paper", "scissors"].sample 
+
+ if @comp_move == "rock"
+  @outcome = "won"
+    elsif @comp_move == "paper"
+    @outcome = "tied"
+     elsif @comp_move =="scissors"
+    @outcome = "lost"
+   end
+
+    render ({:template => "game_templates/user_paper.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  get("", { :controller => "application", :action => "homepage" })
+  get("/", { :controller => "application", :action => "homepage" })
 
 get("/rock", { :controller => "application", :action => "play_rock"})
+
+get("/paper", { :controller => "application", :action => "play_paper"})
 
 end
